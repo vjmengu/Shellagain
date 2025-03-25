c7a8a3cd517c1a239f418df5854947a3fdb4271c
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-25 17:43:14 +0530

    variables

---
+++ 07-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MOVIES=('rrr' 'badri' 'simha')
+echo "The 1st movie is ${MOVIES[0]}"
+echo "The 2nd movie is ${MOVIES[1]}"
+echo "The 3rd movie is ${MOVIES[0]}"
+echo "The list of movies is ${MOVIES[@]}"
