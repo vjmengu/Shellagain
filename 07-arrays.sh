a5c89566b5a030db9326aa5e419788160b4192ab
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-25 17:51:25 +0530

    specialvariables

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,4 +3,4 @@ MOVIES=('rrr' 'badri' 'simha')
 echo "The 1st movie is ${MOVIES[0]}"
 echo "The 2nd movie is ${MOVIES[1]}"
 echo "The 3rd movie is ${MOVIES[0]}"
-echo "The list of movies is ${MOVIES[@]}"
+echo "The list of movies are ${MOVIES[@]}"
---
+++ 08-specialvariables.sh
@@ -0,0 +1,9 @@
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0 "
+echo "Present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$ "
+sleep 60 &
+echo "Process id of last command in background: $!"
