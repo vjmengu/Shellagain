c338f97ad15a340c684fde0bfce43459a948273a
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-26 21:56:52 +0530

    installloops

---
+++ 13-install-using-loops.sh
@@ -0,0 +1,21 @@
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo -e "$(\e[31m) login as a root user"
+else
+
+    for package in $@
+    do
+        dnf list installed $package
+        if [ $? -ne 0 ]
+        then
+            dnf install $package -y
+            if [ $? -ne 0 ]
+            then
+                echo " $package not installed "
+            else
+                echo " $package installation successful "
+            fi
+        else
+            echo " $package already installed "
+    done
