a253f7aa01c06223446cc988a6dd0705b5549a1b
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-26 21:39:40 +0530

    added logs feature

--- 11-function.sh
+++ 11-function.sh
@@ -7,10 +7,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+file_name=$(echo $0 | cut -d "." -f1)
+file_location="/var/log/ShellLogs"
+file_name_logs="$file_location/$file_name-$timestamp.log"
+
 validation(){
     if [ $1 -ne 0 ]
     then
-        dnf install $2 -y
+        dnf install $2 -y &>>$file_name_logs
         if [ $? -ne 0 ]
         then
             echo -e "installing $2... $R failed! $N"
@@ -23,15 +28,16 @@ validation(){
     
     fi
 }
+echo "program excution started at $timestamp" &>>$file_name_logs
 if [ $USERID -ne 0 ]
 then
     echo -e " $R You need root access to perform this task $N "
     exit 1
 
 else
-    dnf list installed mysql -y
+    dnf list installed mysql -y &>>$file_name_logs
     validation $? "mysql"
-    dnf list installed git -y
+    dnf list installed git -y &>>$file_name_logs
     validation $? "git"
 fi
 
