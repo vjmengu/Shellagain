7ccf5413fd0ecec94e3d4c3fe16792ab1531a18e
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-27 22:57:44 +0530

    oldlogs

--- 14-delete-oldlogs.sh
+++ 14-delete-oldlogs.sh
@@ -3,3 +3,12 @@ source_file="/home/ec2-user/logs"
 filestodelete=$(find ${source_file} -name "*.log" -mtime +14)
 
 echo "files to delete are: $filestodelete"
+
+while read -r files
+do
+
+    echo "deleting $files"
+
+    rm -rf $files
+
+done <<< $filestodelete
