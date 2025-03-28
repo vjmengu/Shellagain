76b6ea646bed77658138f0ecf5bece3032932724
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-28 19:40:57 +0530

    mysql.sh

---
+++ expense-shell/mysql.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$R $2 .... Failed $N "
+    else
+        echo "$G $2 ..... Success $N
+    fi
+}
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo -e "$R Please use root access $N"
+    exit 1
+else
+
+     dnf install mysql-server -y
+    validate $? "mysql-server"
+    systemctl enable mysqld
+    validate $? "enable mysql-server"
+    systemctl start mysqld
+    validate $? "starting mysql-server"
+    mysql -h mysql.vijayjohn.online -u root -pExpenseApp@1 -e 'show databases;'
+    if [ $? -ne 0 ]
+    then
+        mysql_secure_installation --set-root-pass ExpenseApp@1
+        validate $? "password setup "
+    else
+        echo "$G Password Already Set!!!, skipping setup $N " 
+    fi
+fi
+
