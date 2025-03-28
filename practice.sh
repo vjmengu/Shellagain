de88be8282492db48ba107cb63921b919bf05b0a
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-28 17:44:11 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -4,13 +4,13 @@ deletef=$(find /home/ec2-user/logs -name "*.log" -mtime +14)
 echo "files to be deleted are $deletef"
 while read -r f 
 do
-    echo " deleting $f "
+    echo -e "\e[31m deleting $f\e[0m "
     rm -rf $f
     if [ $? -ne 0 ]
     then
-        echo "$f deletion failure"
+        echo -e "$f \e[31m deletion failure \e[0m "
     else
-        echo "file deleted : $f"
+        echo -e "\e[32m file deleted : \e[0m  $f"
     fi
 
 done <<< $deletef
