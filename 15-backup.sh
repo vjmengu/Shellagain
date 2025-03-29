ede05c5834c1527715755ccc30e5cebd6371ff23
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-29 21:02:46 +0530

    changes

--- 15-backup.sh
+++ 15-backup.sh
@@ -1,16 +1,46 @@
 #user should give source & destination
-#check is source & dest actually exists
+#check if source & dest actually exists
 #check if any files available on source
 #check if any  log files older than 14 days in source
 #zip
 #check if ziped
 #delete source old log files
+
+R="\e[31m"
+Y="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
+source_dir=$1
+destin_dir=$2
+days={$3:-14}
 usage()
 {
-    echo -e "\e[31m usage:: 15-backup.sh <source files path> <destination file path>"
+    echo -e "\e[31m usage:: 15-backup.sh <source files path> <destination file path> \e[0m"
 }
 if [ $# -lt 2 ]
 then
     usage
+    exit
 fi
+
+if [ ! -d $source_dir ]
+then
+    echo -e " $R $source_dir does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $destin_dir ]
+then
+    echo -e " $R $destin_dir does not exist $N"
+    exit 1
+fi
+
+files=$(find $source_dir -name "*.log" -mtime +$days)
+
+echo -e "$R files older than $days are:: $files $N"
+
+
+
+
 
