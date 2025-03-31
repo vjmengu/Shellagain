ec721c2d7cdd06c0d45a05c2bfed9f2aae01d5ad
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-03-31 22:01:18 +0530

    DiskUsage

---
+++ 17-script1.sh
@@ -0,0 +1,6 @@
+a="I am from script 1 variable a"
+b="I am from script 2 variable b"
+source ./18-script2.sh
+
+echo "$a"
+echo "$b"
---
+++ 18-script2.sh
@@ -0,0 +1,2 @@
+a="I am from script 2 variable a"
+b="I am from script 2 variable b"
